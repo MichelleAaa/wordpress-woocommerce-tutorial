9e4ae4bcd4eb7870c6f83da3dda2cd7c14aeb042
MichelleAaa <99776009+MichelleAaa@users.noreply.github.com>
2023-03-28 19:38:48 -0400

    Homepage Products List - Including Deal of Week with Customizer

--- template-home.php
+++ template-home.php
@@ -58,21 +58,84 @@ get_header(); ?>
 					  </ul>
 					</div>
 				</section>
+				<?php if( class_exists( 'WooCommerce' ) ): ?>
 				<section class="popular-products">
+					<!-- To allow the user to have control we created settings in the customizer.php file. -->
+					<?php 
+					// The first value is the one to use (which is from the customizer, that the user will enter in wp-admin - appearance - customize --- if the user doesn't enter a value, the second parameter is a default value.)
+					$popular_limit		= get_theme_mod( 'set_popular_max_num', 4 );
+					$popular_col 		= get_theme_mod( 'set_popular_max_col', 4 );
+					$arrivals_limit		= get_theme_mod( 'set_new_arrivals_max_num', 4 );
+					$arrivals_col		= get_theme_mod( 'set_new_arrivals_max_col', 4 );
+
+					?>
 					<div class="container">
-						<div class="row">Popular Products</div>
+						<h2>Popular Products</h2>
+						<?php echo do_shortcode( '[products limit=" ' . $popular_limit . ' " columns=" ' . $popular_col . ' " orderby="popularity"]' ); ?>
 					</div>
 				</section>
 				<section class="new-arrivals">
 					<div class="container">
-						<div class="row">New Arrivals</div>
+						<h2>New Arrivals</h2>
+						<?php echo do_shortcode( '[products limit=" ' . $arrivals_limit . ' " columns=" ' . $arrivals_col . ' " orderby="date"]' ); ?>
 					</div>
 				</section>
+				<?php 
+// Gets the value from the wp-admin - appearance- customize section. If there's no value, then the second default will be used.
+				$showdeal			= get_theme_mod( 'set_deal_show', 0 );
+				$deal 				= get_theme_mod( 'set_deal' );
+				$currency			= get_woocommerce_currency_symbol();
+				// in adminer or phpmyadmin - wp_postmeta table, this field is called _regular_price.
+				$regular			= get_post_meta( $deal, '_regular_price', true );
+				$sale 				= get_post_meta( $deal, '_sale_price', true );
+
+				
+				// This section will only display if the checkbox is marked and if the user has typed in any id for the featured product field.
+				if( $showdeal == 1 && ( !empty( $deal ) ) ):
+					// Note that the product must be on sale or else this would result in an error:
+					$discount_percentage = absint( 100 - ( ( $sale/$regular ) * 100 ) );
+				?>
 				<section class="deal-of-the-week">
 					<div class="container">
-						<div class="row">Deal of the Week</div>
+						<h2>Deal of the Week</h2>
+						<div class="row d-flex align-items-center">
+							<div class="deal-img col-md-6 col-12 ml-auto text-center">
+								<!-- large is for the image size. (large is automatically included in wp.) -->
+								<?php echo get_the_post_thumbnail( $deal, 'large', array( 'class' => 'img-fluid' ) ); ?>
+							</div>
+							<div class="deal-desc col-md-4 col-12 mr-auto text-center">
+								<?php if( !empty( $sale ) ): ?>
+									<span class="discount">
+										<?php echo $discount_percentage . '% OFF'; ?>
+									</span>
+								<?php endif; ?>
+								<h3>
+									<a href="<?php echo get_permalink( $deal ); ?>"><?php echo get_the_title( $deal ); ?></a>
+								</h3>
+								<p><?php echo get_the_excerpt( $deal ); ?></p>
+								<div class="prices">
+									<span class="regular">
+										<?php 
+										echo $currency;
+										echo $regular;
+										?>
+									</span>
+									<?php if( !empty( $sale ) ): ?>
+										<span class="sale">
+											<?php 
+											echo $currency;
+											echo $sale;
+											?>				
+										</span>
+									<?php endif; ?>
+								</div>
+								<a href="<?php echo esc_url( '?add-to-cart=' . $deal ); ?>" class="add-to-cart">Add to Cart</a>
+							</div>
+						</div>
 					</div>
 				</section>
+					<?php endif; ?>
+				<?php endif; ?>
 				<section class="lab-blog">
 					<div class="container">
 						<div class="row">
