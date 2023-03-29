fbc3b8f5bc61c4dcf64697c78828423fb0ca6b5b
MichelleAaa <99776009+MichelleAaa@users.noreply.github.com>
2023-03-29 15:45:59 -0400

    Modifying Blog Post index.php

--- index.php
+++ index.php
@@ -25,9 +25,34 @@ get_header();
 								// Load posts loop
 								while( have_posts() ): the_post();
 									?>
-										<article>
-											<h2><?php the_title(); ?></h2>
-											<div><?php the_content(); ?></div>
+									<!-- post_class shows a bunch of classes that you can use to style your post. -->
+										<article <?php post_class(); ?>>
+											<h2>
+												<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+											</h2>
+											<div class="post-thumbnail">
+												<?php 
+												if( has_post_thumbnail() ):
+													the_post_thumbnail( 'fancy-lab-blog', array( 'class' => 'img-fluid' ) );
+												endif;
+												?>
+											</div>
+											<div class="meta">
+												<p>Published by <?php the_author_posts_link(); ?> on <?php echo get_the_date(); ?>
+												<br />
+												<!-- The ' ' below will separate the categories with a space. -->
+												<?php if( has_category() ): ?>
+													Categories: <span><?php 
+														the_category( ' ' ); ?></span>
+												<?php endif; ?>
+												<br />
+												<!-- The first parameter '', shows up at the beginning of the list. The second parameter of ', ' is the tag separator, between each tag. -->
+												<?php if( has_tag() ): ?>
+													Tags: <span><?php the_tags( '', ', ' ); ?></span>
+												<?php endif; ?>
+												</p>
+											</div>
+											<div><?php the_excerpt(); ?></div>
 										</article>
 									<?php
 								endwhile;
